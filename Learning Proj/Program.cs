e3b56e334c97f441a15bd898532ab1efe26b4e19
Kylen Jacob <kylenj06@gmail.com>
2022-08-25 11:05:20 +0700

    finished challenges

--- Learning Proj/Program.cs
+++ Learning Proj/Program.cs
@@ -1,5 +1,124 @@
-﻿console.writeline("Yellow Sofa");
-console.writeline("Yellow Sofa");
-console.writeline("Yellow Sofa");
+﻿//chalenge 1
+//string Name = "Kylen";
+//Console.WriteLine("Hello " + Name);
 
-jhj
+
+//challenge 3
+//string name = Console.ReadLine();
+//Console.WriteLine("hello " + name);
+
+
+//challenge 4
+//int X = 10;
+//int Y = 20;
+//Console.WriteLine(X + Y);
+
+
+
+//challenge 5
+//double X = 5;
+//double Y = 2;
+//Console.WriteLine(X / Y);
+
+
+
+//challenge 6
+//string C = Console.ReadLine();
+//double cel = Convert.ToDouble(C);
+//double F = (cel * 1.8) + 32;
+//Console.WriteLine(C + " celsius in fahrenheit is " + F);
+
+
+//challenge 7
+//Console.WriteLine("enter age");
+//string input = Console.ReadLine();
+//int age = Convert.ToInt32(input);
+//if(age > 18)
+//{
+//    Console.WriteLine("You are an adult");
+//}
+//{
+//    Console.WriteLine("you are young");
+
+//}
+
+
+//challenge 8
+//using Microsoft.Win32.SafeHandles;
+
+//string num1 = Console.ReadLine();
+//string num2 = Console.ReadLine();
+//int out1 = Convert.ToInt32(num1);
+//int out2 = Convert.ToInt32(num2);
+//if((out1 > 0 && out2 < 0) || (out1 < 0 && out2 > 0))
+//{
+//    Console.WriteLine("NC one bro");
+//}
+//else
+//{
+//    Console.WriteLine("nah bruh");
+//}
+
+
+//challenge 9
+//string num1 = Console.ReadLine();
+//string num2 = Console.ReadLine();
+//int alt1 = Convert.ToInt32(num1);
+//int alt2 = Convert.ToInt32(num2);
+//if ((alt1 >= -10 && alt1 <= 10)
+//||
+//(alt2 >= -10 && alt2 <= 10))
+//    {
+//    Console.WriteLine("NC 1 bro");
+//}
+//else
+//{
+//    Console.WriteLine("nah");
+//}
+
+
+//challenge 10/11
+
+
+//for (int count = 1; count < 50; count = count + 2)
+// {
+//    Console.WriteLine(count);
+//}
+
+
+//challenge 12
+
+//for (int count = -300; count < -29; count = count + 3)
+//{
+//    Console.WriteLine(count);
+//}
+
+
+//challenge 13
+
+//int magicnum = 7;
+//int ans = 93570347;
+//while (ans != magicnum)
+//{
+//   string input = Console.ReadLine();
+//    int inputconcersion = Convert.ToInt32(input);
+//    ans = inputconcersion;
+
+//}
+//Console.WriteLine("NC 1 bro");
+
+
+//chalenge 14
+
+//string firstname = "Lebron";
+//string surename = "James";
+//while (firstname != "Santa" && surename != "Claus")
+//{
+//    Console.WriteLine("Who brings you presents for christmas");
+//    string firstname1 = Console.ReadLine();
+//    string surename2 = Console.ReadLine();
+//    firstname = firstname1;
+//    surename = surename2;
+
+//}
+//Console.WriteLine("IKR hes such a good guy");
